a48a4b1e5b1d550c2f544a2dad85abbbe7f19a04
h fam <h fam>
2017-06-17 19:27:03 -0600

    Stuff pushing to firebase but still only one user....working on bug

--- script/game.js
+++ script/game.js
@@ -8,50 +8,63 @@ var config = {
     messagingSenderId: "401672091861"
   };
   firebase.initializeApp(config);
-  var database = firebase.database();
-  console.log(database);
+  var databaseRef = firebase.database();
+  var databaseRefPOne = firebase.database().ref("/Player/One");
+  var databaseRefPTwo = firebase.database().ref("/Player/Two");
+  var databaseRefChat = firebase.database().ref("/Chat");
+  
 
 
 // Definition of Variables
 var userChoice; //Choice made R, P, S
 var usernameInput; //Name user enters
-var playerOne = {};
-var playerTwo = {};
 var defaultUserName = "Guest";
+var wins = 0;
+var losses = 0;
 
+// Do something when submit is clicked
+// - Update firebase database
+// - Update local html "Hi Pavan you are player 1"
+// -- Overwrite input and submit with this text div
 
+// Update page when value of snapshot changes
+// - Updates player boxes
 
 // Actual JS
-// These function control player names
-function generatePlayerNameOne () {
-$("#nameSubmission").click(function (){
-	if (($("#nameInput").val().trim()) === "") {
-		usernameInput = defaultUserName
-		$("#nameArea").html("<h3>" + usernameInput + " You are Player 1</h3>");
-	$("#nameSpotLeft").html(usernameInput);
-	}
-	else {
-usernameInput = $("#nameInput").val().trim();
-$("#nameArea").html("<h3>" + usernameInput + " You are Player 1</h3>");
-$("#nameSpotLeft").html(usernameInput);
+// This simply grabs the username input and sets it to a variable
+function getUserName () {
+	$("#nameSubmission").click(function (){
+		if (($("#nameInput").val().trim()) === "") {
+			usernameInput = defaultUserName
+			
+			}
+			else {
+			usernameInput = $("#nameInput").val().trim();
+			
+			};
+			console.log(usernameInput);
+			shouldWeAddAnother();
+	});
 };
-generateChoices();
-talkShitGetHit(usernameInput);
-});
-};
-function generatePlayerNameTwo () {
-$("#nameSubmission").click(function (){
-	if (($("#nameInput").val().trim()) === "") {
-		usernameInput = defaultUserName
-		$("#nameArea").html("<h3>" + usernameInput + " You are Player 2</h3>");
-	$("#nameSpotRight").html(usernameInput);
-	}
-	else {
-usernameInput = $("#nameInput").val().trim();
-$("#nameArea").html("<h3>" + usernameInput + " You are Player 2</h3>");
-$("#nameSpotRight").html(usernameInput);
-};
-});
+;
+function shouldWeAddAnother () {
+			if (databaseRef.ref("/Player/One").name === undefined) {
+			databaseRefPOne.set({
+			name: usernameInput,
+			wins: 0,
+			losses: 0
+			});
+			}
+			else if (databaseRef.ref("/Player/Two").name === undefined){
+			databaseRefPTwo.set({
+			name: usernameInput,
+			wins: 0,
+			losses: 0
+			});
+			}
+			else {
+			alert("Sorry there are already two players.  You will have to wait your turn.");
+			}	
 };
 // This function will control chat
 function talkShitGetHit (user) {
@@ -71,45 +84,59 @@ function generateChoices () {
 	$(".choicesToShow").html(choicesToShow);
 	$(".choices").click(function () {
 	userChoice = $(this).attr("data-choice");
-	console.log(userChoice);
-	switch (userChoice) {
-		case "rock":
-		$(".choicesToShow").html("<img alt='rock' src='images/rock.png' class='img-responsive col-xs-7 col-xs-offset-2 col-sm-7 col-sm-offset-2 col-md-7 col-md-offset-2 col-lg-7 col-lg-offset-2'>")
-		break;
-		case "paper":
-		$(".choicesToShow").html("<img alt='paper' src='images/paper.png' class='img-responsive col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2'>")
-		break;
-		case "scissors":
-		$(".choicesToShow").html("<img alt='scissors' src='images/scissors.png' class='img-responsive col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2'>")
-		break;
-		default:
-		alert("nothing chosen");
-	}
+		switch (userChoice) {
+			case "rock":
+			$(".choicesToShow").html("<img alt='rock' src='images/rock.png' class='img-responsive col-xs-7 col-xs-offset-2 col-sm-7 col-sm-offset-2 col-md-7 col-md-offset-2 col-lg-7 col-lg-offset-2'>")
+			break;
+			case "paper":
+			$(".choicesToShow").html("<img alt='paper' src='images/paper.png' class='img-responsive col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2'>")
+			break;
+			case "scissors":
+			$(".choicesToShow").html("<img alt='scissors' src='images/scissors.png' class='img-responsive col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2'>")
+			break;
+			default:
+			alert("nothing chosen");
+		}
 	});
 	
 };
-
-// This function will display the chosen image for both players
-
-
-
-
-// This function uses a switch statement to determine who wins
-
-
-
 
 
 $(document).ready(function(){
 $("#nameSpotLeft").html("Waiting on Other Player");
 $("#nameSpotRight").html("Waiting on Other Player");
-$("#score").html('<p id="keepInPlace">Wins: <span id="wins">0          </span>Losses: <span id="losses">0 </span>       </p>');
-generatePlayerNameOne();
-
-
-
-
+getUserName();
 
 
 
 });
+
+
+
+
+
+
+
+
+// Listen even info to add back in once I get data storing properly.
+// if (!(databaseRef.hasChild("PlayerOne"))) {}
+// else if (databaseRef.hasChild("PlayerOne")) {}
+// 		else {
+// 		console.log("3rd");
+// 		alert("Too many players! - Sorry");
+// 	}	
+
+
+
+
+// 		$("#nameArea").html("<h3>" + usernameInputOne + " You are Player 1</h3>");
+// 		$("#nameSpotLeft").html(usernameInputOne);
+
+// 		$("#nameArea").html("<h3>" + usernameInputOne + " You are Player 1</h3>");
+// 		$("#nameSpotLeft").html(usernameInputOne);
+
+// 		$("#nameArea").html("<h3>" + usernameInputTwo + " You are Player 2</h3>");
+// 		$("#nameSpotLeft").html(usernameInputTwo);
+// 		$("#nameArea").html("<h3>" + usernameInputTwo + " You are Player 2</h3>");
+// 		$("#nameSpotLeft").html(usernameInputTwo);
+
